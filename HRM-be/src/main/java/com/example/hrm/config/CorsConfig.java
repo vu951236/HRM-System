2f5c89116f2c8bc3b8fe8ea4c67e4fbab2101a5f
vu951236 <hoangvuvo907@gmail.com>
2025-08-29 21:55:56 +0700

    fix bug cors

--- HRM-be/src/main/java/com/example/hrm/config/CorsConfig.java
+++ HRM-be/src/main/java/com/example/hrm/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.example.hrm.config;
 
-import lombok.experimental.NonFinal;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,24 +12,21 @@ import java.util.Arrays;
 @Configuration
 public class CorsConfig {
 
-    @NonFinal
-    @Value("${DOMAIN_FE:https://192.168.1.11:3000}")
-    private String DomainFe;
+    @Value("${DOMAIN_FE}")
+    private String domainFe; 
 
     @Bean
     public CorsFilter corsFilter() {
-
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
         corsConfiguration.addAllowedOrigin("http://localhost:5173");
-        corsConfiguration.addAllowedOrigin(DomainFe);
+        corsConfiguration.addAllowedOrigin(domainFe);
         corsConfiguration.addAllowedHeader("*");
         corsConfiguration.setExposedHeaders(Arrays.asList("Set-Cookie"));
         corsConfiguration.addAllowedMethod("*");
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfiguration);
-
         return new CorsFilter(source);
     }
 }
