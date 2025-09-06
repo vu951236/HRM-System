8bb6af6f23fdca88280c043987bc1d361c1b0672
vu951236 <hoangvuvo907@gmail.com>
2025-09-06 17:05:27 +0700

    fix bug

--- HRM-be/src/main/java/com/example/hrm/repository/DeviceRepository.java
+++ HRM-be/src/main/java/com/example/hrm/repository/DeviceRepository.java
@@ -3,7 +3,6 @@ package com.example.hrm.repository;
 import com.example.hrm.entity.Device;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.lang.ScopedValue;
 import java.util.Optional;
 
 public interface DeviceRepository extends JpaRepository<Device, Long> {
