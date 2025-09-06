61b6de12c1e94092c988c41b7377f2b098c6c169
vu951236 <hoangvuvo907@gmail.com>
2025-09-06 17:49:09 +0700

    update ApplicationInit

--- HRM-be/src/main/java/com/example/hrm/config/ApplicationInitConfig.java
+++ HRM-be/src/main/java/com/example/hrm/config/ApplicationInitConfig.java
@@ -2,8 +2,10 @@ package com.example.hrm.config;
 
 import com.example.hrm.entity.Role;
 import com.example.hrm.entity.User;
+import com.example.hrm.entity.EmployeeProfile;
 import com.example.hrm.repository.RoleRepository;
 import com.example.hrm.repository.UserRepository;
+import com.example.hrm.repository.EmployeeProfileRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
@@ -15,6 +17,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.time.LocalDate;
+
 @Configuration
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -23,6 +27,7 @@ public class ApplicationInitConfig {
 
     PasswordEncoder passwordEncoder;
     RoleRepository roleRepository;
+    EmployeeProfileRepository employeeProfileRepository;
 
     @NonFinal
     static final String ADMIN_USER_NAME = "admin";
@@ -53,7 +58,21 @@ public class ApplicationInitConfig {
                         .build();
 
                 userRepository.save(user);
-                log.warn("Admin user has been created with default password: admin, please change it!");
+
+                EmployeeProfile profile = EmployeeProfile.builder()
+                        .user(user)
+                        .fullName("ADMIN")
+                        .gender("Nam")
+                        .dob(LocalDate.of(2000, 1, 1))
+                        .phone("0123456789")
+                        .address("System Default")
+                        .imageUrl("https://static.vecteezy.com/system/resources/previews/009/398/577/original/man-avatar-clipart-illustration-free-png.png")
+                        .joinedDate(LocalDate.now())
+                        .build();
+
+                employeeProfileRepository.save(profile);
+
+                log.warn("Admin user has been created with default profile & password: admin, please change it!");
             }
             log.info("Application initialization completed.");
         };
