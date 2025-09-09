9957150d57e5b2899f4782d2afd353222cb567e4
vu951236 <hoangvuvo907@gmail.com>
2025-09-09 22:35:24 +0700

    update to run both deploy and local

---
+++ HRM-be/src/main/java/com/example/hrm/config/CookieProperties.java
@@ -0,0 +1,16 @@
+package com.example.hrm.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Setter
+@Getter
+@Configuration
+@ConfigurationProperties(prefix = "app.cookie")
+public class CookieProperties {
+    private boolean secure;
+    private String sameSite;
+
+}
--- HRM-be/src/main/java/com/example/hrm/controller/AuthenticationController.java
+++ HRM-be/src/main/java/com/example/hrm/controller/AuthenticationController.java
@@ -1,5 +1,6 @@
 package com.example.hrm.controller;
 
+import com.example.hrm.config.CookieProperties;
 import com.example.hrm.dto.request.AuthenticationRequest;
 import com.example.hrm.dto.request.IntrospectRequest;
 import com.example.hrm.dto.response.ApiResponse;
@@ -17,12 +18,13 @@ import org.springframework.web.bind.annotation.*;
 
 import java.text.ParseException;
 
-@RequiredArgsConstructor
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
+    private final CookieProperties cookieProperties;
 
     @PostMapping("/login")
     public ResponseEntity<ApiResponse<AuthenticationResponse>> login(
@@ -31,18 +33,16 @@ public class AuthenticationController {
 
         AuthenticationResponse authenticationResponse = authenticationService.login(request);
 
-        ResponseCookie refreshCookie = ResponseCookie.from("refresh_token", authenticationResponse.getRefreshToken())
-                .secure(true)
-                .httpOnly(true)
-                .sameSite("None")
-                .maxAge(30 * 24 * 60 * 60)
-                .path("/")
-                .build();
+        ResponseCookie refreshCookie = buildCookie(
+                authenticationResponse.getRefreshToken(),
+                30 * 24 * 60 * 60 
+        );
 
         response.setHeader(HttpHeaders.SET_COOKIE, refreshCookie.toString());
 
         return ResponseEntity.ok(ApiResponse.<AuthenticationResponse>builder()
                 .data(authenticationResponse)
+                .message("Đăng nhập thành công")
                 .build());
     }
 
@@ -54,22 +54,18 @@ public class AuthenticationController {
 
         authenticationService.logout(request, refreshToken);
 
-        ResponseCookie clearCookie = ResponseCookie.from("refresh_token", "")
-                .secure(true)
-                .httpOnly(true)
-                .sameSite("None")
-                .maxAge(0)
-                .path("/")
-                .build();
-
+        ResponseCookie clearCookie = buildCookie("", 0);
         response.setHeader(HttpHeaders.SET_COOKIE, clearCookie.toString());
 
-        return ResponseEntity.ok(ApiResponse.<Void>builder().build());
+        return ResponseEntity.ok(ApiResponse.<Void>builder()
+                .message("Đăng xuất thành công")
+                .build());
     }
 
     @PostMapping("/refresh-token")
     public ResponseEntity<ApiResponse<AuthenticationResponse>> refreshToken(
-            @CookieValue(name = "refresh_token", required = false) String refreshToken) throws ParseException, JOSEException {
+            @CookieValue(name = "refresh_token", required = false) String refreshToken
+    ) throws ParseException, JOSEException {
 
         if (refreshToken == null) {
             throw new AppException(ErrorCode.INVALID_REFRESH_TOKEN);
@@ -77,18 +73,27 @@ public class AuthenticationController {
 
         AuthenticationResponse newAccessToken = authenticationService.refreshAccessToken(refreshToken);
 
-        ResponseCookie refreshCookie = ResponseCookie.from("refresh_token", newAccessToken.getRefreshToken())
-                .secure(true)
-                .httpOnly(true)
-                .sameSite("None")
-                .maxAge(30 * 24 * 60 * 60)
-                .path("/")
-                .build();
+        ResponseCookie refreshCookie = buildCookie(
+                newAccessToken.getRefreshToken(),
+                30 * 24 * 60 * 60
+        );
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, refreshCookie.toString())
                 .body(ApiResponse.<AuthenticationResponse>builder()
                         .data(newAccessToken)
+                        .message("Cập nhật access token thành công")
                         .build());
+    }
+
+
+    private ResponseCookie buildCookie(String value, long maxAge) {
+        return ResponseCookie.from("refresh_token", value)
+                .httpOnly(true)
+                .secure(cookieProperties.isSecure())
+                .sameSite(cookieProperties.getSameSite())
+                .path("/")
+                .maxAge(maxAge)
+                .build();
     }
 }
